fb4e5a0735d03018ae87bff56f3ccd454f98e476
victor-grajski <victor.grajski@gmail.com>
2020-10-25 16:58:35 -0400

    order status HTML

--- src/components/OrderStatus/OrderStatus.js
+++ src/components/OrderStatus/OrderStatus.js
@@ -6,6 +6,10 @@ import './OrderStatus.css';
 
 import paw from '../../assets/icons/paw.svg';
 import cart from '../../assets/icons/cart.svg';
+import checkFull from '../../assets/icons/check-full.svg';
+import checkEmpty from '../../assets/icons/check-empty.svg';
+
+import dogHarness from '../../assets/photos/dog-harness.jpg';
 
 const OrderStatus = () => {
     const context = useContext(Context);
@@ -39,109 +43,64 @@ const OrderStatus = () => {
                 </NavLink>
             </header>
 
-            <div className="order-status-container drop-shadow">
-                <div className="confirmation-title">Thank you, your order has been placed.</div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
+            <div className="order-status-container-container drop-shadow">
+                <div className="order-status-container">
+                    <div className="order-status-item">
+                        <div className="order-status-label">Order Placed</div>
+                        <img src={checkFull} className="order-status-icon" alt="check full" />
+                        <div className="order-status-time">Wed Sep 23</div>
                     </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
+                    <div className="order-status-item">
+                        <div className="order-status-label">Shipped</div>
+                        <img src={checkEmpty} className="order-status-icon" alt="check empty" />
+                        <div className="order-status-time">Fri Sep 25</div>
                     </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
+                    <div className="order-status-item">
+                        <div className="order-status-label">Delivered</div>
+                        <img src={checkEmpty} className="order-status-icon" alt="check empty" />
+                        <div className="order-status-time">Sun Sep 27</div>
                     </div>
                 </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
+                <div className="order-info-container">
+                    <div className="order-shipping-address-container">
+                        <div className="order-shipping-address-title">Shipping Address</div>
+                        <div className="order-shipping-address-name">Victor Grajski</div>
+                        <div className="order-shipping-address-line1">5000 Forbes Avenue #123</div>
+                        <div className="order-shipping-address-line2">Pittsburgh, PA 15217</div>
                     </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="confirmation-info-container">
-                    <div className="confirmation-info">
-                        <div className="confirmation-label">{numItems} items will be shipped to Victor Grajski</div>
-                        <div className="confirmation-address">5000 Forbes Avenue, #123 Pittsburgh, PA 15217</div>
-                        <div className="delivery-date">Estimated delivery: Sunday, Sep 27</div>
-                    </div>
-                    <div className="confirmation-image-container">
-                        <img src={product.image} className="confirmation-image drop-shadow" alt="product" />
-                    </div>
-                </div>
-                <div className="signup-container">
-                    <div className="signup-title">Sign up for email alerts</div>
-                    <div className="signup-input-container">
-                        <div className="signup-input-label">Email address</div>
-                        <div className="signup-input">
-                            <input type="text" className="signup-input-text" id="email-signup-input-text" name="signup-input-text" />
+                    <div className="order-total-container">
+                        <div className="order-total-left">
+                            <div className="order-status-total">Order Total</div>
+                            <div className="order-status-subtotal">Subtotal</div>
+                            <div className="order-status-shipping">Shipping</div>
+                            <div className="order-status-tax">Tax</div>
+                        </div>
+                        <div className="order-total-right">
+                            <div className="order-status-total-amount">$35.42</div>
+                            <div className="order-status-subtotal-amount">$24.99</div>
+                            <div className="order-status-shipping-amount">$8.99</div>
+                            <div className="order-status-tax-amount">$1.44</div>
                         </div>
                     </div>
-                    <div className="signup-button">Sign up</div>
                 </div>
-                <div className="signup-container" id="sms-signup-container">
-                    <div className="signup-title">Sign up for SMS alerts</div>
-                    <div className="signup-input-container">
-                        <div className="signup-input-label">Phone Number</div>
-                        <div className="signup-input">
-                            <input type="text" className="signup-input-text" id="sms-signup-input-text" name="signup-input-text" />
+                <div className="order-items-container">
+                    <div className="order-items-title">Items</div>
+                    <div className="order-status-items-container">
+                        <div className="order-status-item-container">
+                            <div className="order-status-item-main">
+                                <img src={dogHarness} className="order-status-item-image" alt="product" />
+                                <div className="order-status-item-info">
+                                    <div className="order-status-item-title">Dog Harness</div>
+                                    <div className="order-status-item-size">Size: Large</div>
+                                    <div className="order-status-item-color">Color: Blackberry</div>
+                                    <div className="order-status-item-quantity">Quantity: 1</div>
+                                </div>
+                                <div className="order-status-item-price">$24.99</div>
+                            </div>
+                            <div className="order-status-item-line"></div>
                         </div>
                     </div>
-                    <div className="signup-button">Sign up</div>
                 </div>
-                <NavLink to={appRoutes.orderStatus} className="check-order-status drop-shadow">Check Order Status</NavLink>
             </div>
         </div>
     );
